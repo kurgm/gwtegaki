f64764519b5b3f3b2cfe808c003a48540c0bc580
kurgm <gogottt2009-hp@yahoo.co.jp>
2020-06-25 00:56:31 +0900

    Load dataset on first request

--- backend/dataset.py
+++ backend/dataset.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import threading
 import time
 
 from search import NGTIndex, read_names_list
@@ -12,24 +11,22 @@ index = None
 names_list = None
 
 
-def load_dataset(datadirpath: str):
+def load_dataset_path(datadirpath: str):
     global index, names_list
     index = NGTIndex(os.path.join(datadirpath, 'anng'), read_only=True)
     names_list = read_names_list(os.path.join(datadirpath, 'names.txt'))
 
 
-def load_dataset_():
-    time.sleep(0.5)
-
+def load_dataset():
     logger.info('dataset load started')
     start_t = time.time()
 
     if os.environ.get('HWR_INDEX_PATH'):
-        load_dataset(os.environ['HWR_INDEX_PATH'])
+        load_dataset_path(os.environ['HWR_INDEX_PATH'])
     else:
         from gcs import get_index_dir
         with get_index_dir() as indexdirpath:
-            load_dataset(indexdirpath)
+            load_dataset_path(indexdirpath)
 
     logger.info('dataset load finished')
     end_t = time.time()
@@ -37,9 +34,6 @@ def load_dataset_():
 
 
 def get_dataset():
-    load_thread.join()
+    if index is names_list is None:
+        load_dataset()
     return index, names_list
-
-
-load_thread = threading.Thread(target=lambda: load_dataset_())
-load_thread.start()
