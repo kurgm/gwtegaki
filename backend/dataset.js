6a467688b9cd74bbdb879aa858c19e079fc75fb2
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2024-03-15 16:16:23 +0900

    [backend] Remove loading dataset from Google Cloud Storage

--- backend/dataset.js
+++ backend/dataset.js
@@ -1,10 +1,6 @@
 // @ts-check
 
-const os = require('os');
-const fs = require('fs');
 const path = require('path');
-
-const { Storage } = require('@google-cloud/storage')
 
 const DATASET_FILES = /** @type {const} */([
   'names.txt',
@@ -59,47 +55,12 @@ class Dataset {
  */
 const createLocalDirDataset = (dirpath) => Promise.resolve(new Dataset(dirpath));
 
-/**
- * @param {string} gcsBucketName
- * @param {string} gcsDir
- * @return {Promise<Dataset>}
- */
-const createGCSDirDataset = async (gcsBucketName, gcsDir) => {
-  const tempPath = fs.mkdtempSync(path.join(os.tmpdir(), 'gwtegakibackend-'));
-  const cleanupTempDir = () => {
-    fs.rmSync(tempPath, { recursive: true });
-  };
-  try {
-    const cloudStorage = new Storage();
-    const bucket = cloudStorage.bucket(gcsBucketName);
-
-    console.log('start downloading dataset to:', tempPath);
-    await Promise.all(DATASET_FILES.map((filepath) => (
-      bucket.file(path.join(gcsDir, filepath)).download({
-        destination: path.join(tempPath, filepath),
-      })
-    )));
-    console.log('complete downloading dataset to:', tempPath);
-  } catch (e) {
-    cleanupTempDir();
-    throw e;
-  }
-  return new Dataset(tempPath, cleanupTempDir);
-};
-
 /** @return {Promise<Dataset>} */
 exports.getDataset = () => {
   const localPath = process.env.HWR_INDEX_PATH;
   if (localPath) {
     console.debug('using local directory dataset:', localPath);
     return createLocalDirDataset(localPath);
-  }
-
-  const gcsBucketName = process.env.INDEX_GCS_BUCKET_NAME;
-  const gcsDir = process.env.INDEX_GCS_DIR;
-  if (gcsBucketName && gcsDir) {
-    console.debug('using GCS directory dataset:', `gs://${gcsBucketName}/${gcsDir}`);
-    return createGCSDirDataset(gcsBucketName, gcsDir);
   }
 
   throw new Error('no dataset available');
