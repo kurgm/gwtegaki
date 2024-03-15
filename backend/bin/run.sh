611984efcdc1ae92ad156a4ca4e24ff02358a3f4
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2024-03-15 15:57:57 +0900

    [backend] Remove GCS Fuse
    Use Cloud Run's volume mounts feature

--- backend/bin/run.sh
+++ backend/bin/run.sh
@@ -2,23 +2,13 @@
 
 set -e
 
-if [ -z "$INDEX_GCS_BUCKET_NAME" ] || [ -z "$INDEX_GCS_DIR" ]; then
-  echo "INDEX_GCS_BUCKET_NAME and INDEX_GCS_DIR are required."
+if [ -z "$INDEX_GCS_DIR" ]; then
+  echo "INDEX_GCS_DIR are required."
   exit 1
 fi
-
-mkdir -p "$GCS_MNT_DIR"
-
-echo "Mounting GCS Fuse."
-gcsfuse --implicit-dirs -o ro "$INDEX_GCS_BUCKET_NAME" "$GCS_MNT_DIR"
-echo "Mounting completed."
 
 echo "Copying dataset to a temporary directory."
 time cp -r "$GCS_MNT_DIR/$INDEX_GCS_DIR" /tmp/dataset
 echo "Copying completed."
-
-echo "Unmounting GCS Fuse."
-fusermount -u "$GCS_MNT_DIR"
-echo "Unmounting completed."
 
 HWR_INDEX_PATH=/tmp/dataset npm start
