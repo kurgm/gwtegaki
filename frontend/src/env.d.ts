584bc2287d2d2614d7ef9929525657d5964644d1
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2025-01-25 16:43:42 +0900

    Update frontend dependencies

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
-
 interface ImportMetaEnv {
   readonly PUBLIC_SEARCH_API_URL: string;
 }
